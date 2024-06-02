9e80a7eb1b09385e93ab4a76cb2c93beec48fd9f
fduran <duran.fernando@gmail.com>
2024-06-02 19:42:00 -0400

    first

---
+++ main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(handler)
+
+	handler.ServeHTTP(rr, req)
+
+	expected := "Hello, you've requested: /\n"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
