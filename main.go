96086c854e4284bfd7d02b46accee1e21f7c93ca
fduran <duran.fernando@gmail.com>
2024-06-02 19:46:40 -0400

    5th

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hey! %s\n", r.URL.Path)
 	// add logging
-	fmt.Printf("Request: %s\n", r.URL.Path)
+	// fmt.Printf("Request: %s\n", r.URL.Path)
 }
 
 func main() {
-	// serve on :8181
+	// web server on :8181
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8181", nil)
 }
